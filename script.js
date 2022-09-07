f421db2b97040cff12591ab7e002bf6c2b254913
dioshusband <89948569+diegobrando0@users.noreply.github.com>
2022-09-07 17:16:51 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,1 @@
+object.onmouseover = function <img src = 'images/door1.png' onmouseover = "images/goat.png';" onmouseout = "images/door1.png';" /> (object.addEventListener("mouseover", myScript);) { myScript };
